df1fe713390433fb40acfbf3a3dfb601f021e708
Tony DiNitto <think41c@gmail.com>
2015-04-27 23:30:04 -0400

    More case statements established.

--- beer_song.rb
+++ beer_song.rb
@@ -4,6 +4,9 @@ class BeerSong
     when 3..99
     "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" \
     "Take one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"
+    when 2
+    "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" \
+    "Take one down and pass it around, #{verse_number-1} bottle of beer on the wall.\n"
     end
   end
 
@@ -14,3 +17,5 @@ class BeerSong
   end
 
 end
+a = BeerSong.new
+p a.verse(2)
