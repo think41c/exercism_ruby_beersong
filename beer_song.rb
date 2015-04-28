f8164c0942e83c8d09e0d08cdf208071479ed318
Tony DiNitto <think41c@gmail.com>
2015-04-27 23:23:00 -0400

    Beginning logic for verse numbers.

--- beer_song.rb
+++ beer_song.rb
@@ -1,7 +1,10 @@
 class BeerSong 
   def verse(verse_number)
-    "8 bottles of beer on the wall, 8 bottles of beer.\n" \
-      "Take one down and pass it around, 7 bottles of beer on the wall.\n"
+    
+    
+
+    "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" \
+    "Take one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"
   end
 
   def verses(high_verse, low_verse)
