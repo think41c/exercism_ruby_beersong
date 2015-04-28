e3683a2e4f65dc7420d1ec9ba46211e107b6068d
Tony DiNitto <think41c@gmail.com>
2015-04-27 23:25:56 -0400

    Case statement used for verse verbage selection.

--- beer_song.rb
+++ beer_song.rb
@@ -1,10 +1,10 @@
 class BeerSong 
   def verse(verse_number)
-    
-    
-
+    case verse_number
+    when 3..99
     "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" \
     "Take one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"
+    end
   end
 
   def verses(high_verse, low_verse)
