df09778e76bc51c1797e2bb3e446f78ac586adba
Tony DiNitto <think41c@gmail.com>
2015-04-27 23:17:42 -0400

    All methods established.

--- beer_song.rb
+++ beer_song.rb
@@ -1,8 +1,13 @@
 class BeerSong 
-  def verse(x)
+  def verse(verse_number)
     "8 bottles of beer on the wall, 8 bottles of beer.\n" \
       "Take one down and pass it around, 7 bottles of beer on the wall.\n"
   end
 
+  def verses(high_verse, low_verse)
+  end
+
+  def sing
+  end
+
 end
-puts "hi"
