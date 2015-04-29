6212b42d189fbb8be01f85fca9daf4e2a59fe3f1
Tony DiNitto <think41c@gmail.com>
2015-04-28 22:35:40 -0400

    Refactoring and cleaning code.

--- beer_song.rb
+++ beer_song.rb
@@ -11,16 +11,16 @@ class BeerSong
     "#{verse_number} bottle of beer on the wall, #{verse_number} bottle of beer.\n" \
     "Take it down and pass it around, no more bottles of beer on the wall.\n"
     when 0 
-      "No more bottles of beer on the wall, no more bottles of beer.\n" \
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    "No more bottles of beer on the wall, no more bottles of beer.\n" \
+    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
   end
 
   def verses(high_verse, low_verse)
     multiple_verses = []  
-    until high_verse == (low_verse - 1)
+    until high_verse < low_verse
       multiple_verses << verse(high_verse) + "\n"
-      high_verse = high_verse - 1
+      high_verse -= 1
     end
     multiple_verses.join
   end
