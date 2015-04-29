a2814452f3d69af16cfe9a35886ec3cebb84d05c
Tony DiNitto <think41c@gmail.com>
2015-04-28 22:41:08 -0400

    DRYing up code in verses.

--- beer_song.rb
+++ beer_song.rb
@@ -1,18 +1,19 @@
 class BeerSong 
+  
   def verse(verse_number)
     case verse_number
     when 3..99
-    "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" \
-    "Take one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"
+    "#{verse_number} #{bottles}, #{verse_number} bottles of beer.\n" \
+    "#{take}#{verse_number-1} #{bottles}.\n"
     when 2
-    "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n" \
-    "Take one down and pass it around, #{verse_number-1} bottle of beer on the wall.\n"
+    "#{verse_number} #{bottles}, #{verse_number} bottles of beer.\n" \
+    "#{take}#{verse_number-1} bottle of beer on the wall.\n"
     when 1
     "#{verse_number} bottle of beer on the wall, #{verse_number} bottle of beer.\n" \
-    "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    "Take it down and pass it around, no more #{bottles}.\n"
     when 0 
-    "No more bottles of beer on the wall, no more bottles of beer.\n" \
-    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    "No more #{bottles}, no more bottles of beer.\n" \
+    "Go to the store and buy some more, 99 #{bottles}.\n"
     end
   end
 
@@ -28,4 +29,15 @@ class BeerSong
   def sing
     verses(99,0)
   end
+
+  private 
+
+  def bottles
+    "bottles of beer on the wall"
+  end
+
+  def take
+    "Take one down and pass it around, "
+  end
+
 end
