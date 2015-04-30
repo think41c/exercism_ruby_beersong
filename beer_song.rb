3d05a101bdd9492e73d5050b8ff62fa8f31ad751
Tony DiNitto <think41c@gmail.com>
2015-04-30 18:10:29 -0400

    Fixed indentation.

--- beer_song.rb
+++ beer_song.rb
@@ -10,23 +10,23 @@ class BeerSong
   def verse(verse_number)
     case verse_number
     when 3..99
-    "#{verse_number} #{bottles}, #{verse_number} bottles of beer.\n" \
-    "#{take}#{verse_number-1} #{bottles}.\n"
+      "#{verse_number} #{bottles}, #{verse_number} bottles of beer.\n" \
+      "#{take}#{verse_number-1} #{bottles}.\n"
     when 2
-    "#{verse_number} #{bottles}, #{verse_number} bottles of beer.\n" \
-    "#{take}#{verse_number-1} bottle of beer on the wall.\n"
+      "#{verse_number} #{bottles}, #{verse_number} bottles of beer.\n" \
+      "#{take}#{verse_number-1} bottle of beer on the wall.\n"
     when 1
-    "#{verse_number} bottle of beer on the wall, #{verse_number} bottle of beer.\n" \
-    "Take it down and pass it around, no more #{bottles}.\n"
+      "#{verse_number} bottle of beer on the wall, #{verse_number} bottle of beer.\n" \
+      "Take it down and pass it around, no more #{bottles}.\n"
     when 0 
-    "No more #{bottles}, no more bottles of beer.\n" \
-    "Go to the store and buy some more, 99 #{bottles}.\n"
+      "No more #{bottles}, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 #{bottles}.\n"
     end
   end
 
   def verses(high_verse, low_verse)
     until high_verse < low_verse
-      @multiple_verses << verse(high_verse) + "\n"
+      @multiple_verses << verse(high_verse)
       high_verse -= 1
     end
     multiple_verses.join
